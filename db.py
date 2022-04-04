dd3e05b40b1133720a6e20727e235b378bcf3364
resqiar <resqiageng@gmail.com>
2022-04-04 23:22:16 +0700

    chore: move database configs to env file

--- db.py
+++ db.py
@@ -1,13 +1,14 @@
+import os
 import psycopg2
 
 
 def db_connection():
     """ function to open connection """
     conn = psycopg2.connect(
-        host='localhost',
-        port=5432,
-        database='flask',
-        user='postgres',
-        password='admin'
+        host=os.environ['PG_HOST'],
+        port=os.environ['PG_PORT'],
+        database=os.environ['PG_DB'],
+        user=os.environ['PG_USER'],
+        password=os.environ['PG_PASSWORD']
     )
     return conn
